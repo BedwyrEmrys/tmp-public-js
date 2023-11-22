f91fc23099213c8d6b5029325f85c014cbfba53a
BedwyrEmrys <bedwyr.emrys@outlook.com>
2023-11-22 21:40:48 +0900

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,23 +1,56 @@
-	function increasePlaybackSpeed() {
-    // Get the video player element
-    const videoPlayer = document.querySelector('.video-stream');
- 
-    // Check if the video player element exists
-    if (videoPlayer) {
-        // Get the current playback speed
-        const currentSpeed = videoPlayer.playbackRate;
- 
-        // Check if the current playback speed is 1 (normal speed)
-        if (currentSpeed === 1) {
-            // Increase the playback speed to 5x
-            videoPlayer.playbackRate = 5;
-            console.log('Playback speed increased to 5x');
-        } else {
-            // Decrease the playback speed back to normal
-            videoPlayer.playbackRate = 1;
-            console.log('Playback speed decreased to normal');
-        }
-    } else {
-        console.error('Video player element not found');
+// ==UserScript==
+// @name         speed2-3x
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.anthropic.com/index/claude-2
+// @match        *://*.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=anthropic.com
+// @grant        none
+// ==/UserScript==
+
+function speedcontrol() {
+    let speed23 = document.getElementById("speed_2-3");
+    if (!speed23) {
+        // 速度级别
+        var speeds = [2.25,2.5,2.75,3];
+
+        // 创建速度按钮
+        var container = document.createElement("div");
+        container.setAttribute("id", "speed_2-3");
+        container.style.cssText = "display: flex;align-items: center;";
+        speeds.forEach(function(x) {
+            var btn = document.createElement("button");
+            btn.textContent = x + "x";
+            btn.onclick = function() {
+                const videoPlayer = document.querySelector('.video-stream');
+                videoPlayer.playbackRate = x;
+                console.log('Playback speed increased to 5x');
+
+            }
+            container.appendChild(btn);
+        });
+
+        // 插入页面
+        var subscribeButton = document.getElementById("owner");
+        subscribeButton.insertAdjacentElement('afterend', container);
+
     }
 }
+
+(function() {
+    'use strict';
+
+
+    // 定时刷新间隔(单位:毫秒)
+    const refreshInterval = 10000;
+
+    setInterval(speedcontrol, refreshInterval);
+
+    // Your code here...
+})();
+
+
+
+
